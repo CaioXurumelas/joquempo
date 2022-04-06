928d757b9fa35769b9a400e02737e667257695ee
CaioXurumelas <47764368+CaioXurumelas@users.noreply.github.com>
2022-04-06 20:24:03 -0300

    Update 02
    Functions about winner

--- Joquempo/index.js
+++ Joquempo/index.js
@@ -9,26 +9,79 @@ const $scissorsButton2 = document.querySelector(".button-scissors-02")
 const $fieldPlayer1 = document.querySelector(".field-01")
 const $fieldPlayer2 = document.querySelector(".field-02")
 
+const $winnerTitle = document.querySelector(".winner-title")
+
+let movePlayer1 = ""
+let movePlayer2 = ""
+
+let gameResult = "null"
+
+function verifyGame() {
+    if (movePlayer1 == "stone" && movePlayer2 == "paper") {
+        gameResult = 2
+    } else if (movePlayer1 == "stone" && movePlayer2 == "scissors") {
+        gameResult = 1
+    } else if (movePlayer1 == "paper" && movePlayer2 == "stone") {
+        gameResult = 1
+    } else if (movePlayer1 == "paper" && movePlayer2 == "scissors") {
+        gameResult = 2
+    } else if (movePlayer1 == "scissors" && movePlayer2 == "stone") {
+        gameResult = 2
+    } else if (movePlayer1 == "scissors" && movePlayer2 == "paper") {
+        gameResult = 1
+    } else if (movePlayer1 == movePlayer2) {
+        gameResult = 0
+    }
+}
+
+function printGameResult() {
+    if (gameResult == 0) {
+        $winnerTitle.innerHTML = "Empatou!"
+    } else if (gameResult == 1) {
+        $winnerTitle.innerHTML = "Jogador 1 Ganhou!"
+    } else if (gameResult == 2) {
+        $winnerTitle.innerHTML = "Jogador 2 Ganhou!"
+    }
+}
+
 $stoneButton1.addEventListener("click", function() {
     $fieldPlayer1.innerHTML = "<img class='move-image' src='./images/stone.png' alt=''>"
+    movePlayer1 = "stone"
+    verifyGame()
+    printGameResult()
 })
 
 $paperButton1.addEventListener("click", function() {
     $fieldPlayer1.innerHTML = "<img class='move-image' src='./images/paper.png' alt=''>"
+    movePlayer1 = "paper"
+    verifyGame()
+    printGameResult()
 })
 
 $scissorsButton1.addEventListener("click", function() {
     $fieldPlayer1.innerHTML = "<img class='move-image' src='./images/scissors.png' alt=''>"
+    movePlayer1 = "scissors"
+    verifyGame()
+    printGameResult()
 })
 
 $stoneButton2.addEventListener("click", function() {
     $fieldPlayer2.innerHTML = "<img class='move-image' src='./images/stone.png' alt=''>"
+    movePlayer2 = "stone"
+    verifyGame()
+    printGameResult()
 })
 
 $paperButton2.addEventListener("click", function() {
     $fieldPlayer2.innerHTML = "<img class='move-image' src='./images/paper.png' alt=''>"
+    movePlayer2 = "paper"
+    verifyGame()
+    printGameResult()
 })
 
 $scissorsButton2.addEventListener("click", function() {
     $fieldPlayer2.innerHTML = "<img class='move-image' src='./images/scissors.png' alt=''>"
+    movePlayer2 = "scissors"
+    verifyGame()
+    printGameResult()
 })
